3729d67f0e8648b57f914af1920ecc31a9eec361
Juliano <souzza.s1990@gmail.com>
2023-05-02 14:25:53 -0300

    configuring packages of react

--- desyng_system/packages/react/dist/index.mjs
+++ desyng_system/packages/react/dist/index.mjs
@@ -1,2 +1,9 @@
-// src/index.ts
-console.log("Ol\xE1 mundo!");
+// src/index.tsx
+import { colors } from "@ignite-ui/colors";
+import { jsx } from "react/jsx-runtime";
+function App() {
+  return /* @__PURE__ */ jsx("h1", { style: { color: colors.ignite300 }, children: "Hello words" });
+}
+export {
+  App
+};
---
+++ desyng_system/packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import { colors } from '@ignite-ui/colors'
+
+export function App() {
+  return <h1 style={{ color: colors.ignite300 }}>Hello words</h1>
+}
