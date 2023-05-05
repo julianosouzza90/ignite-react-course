4dc7abd0af72ded963ed0526cbaf3460942f0c6f
Juliano Souza <souzza.s1990@gmail.com>
2023-05-05 18:27:45 -0300

    feat(create-turbo): apply official-starter transform

--- desyng_system/packages/react/dist/index.mjs
+++
@@ -1,9 +0,0 @@
-// src/index.tsx
-import { colors } from "@ignite-ui/colors";
-import { jsx } from "react/jsx-runtime";
-function App() {
-  return /* @__PURE__ */ jsx("h1", { style: { color: colors.ignite300 }, children: "Hello words" });
-}
-export {
-  App
-};
--- desyng_system/packages/react/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import { colors } from '@ignite-ui/colors'
-
-export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Hello words</h1>
-}
